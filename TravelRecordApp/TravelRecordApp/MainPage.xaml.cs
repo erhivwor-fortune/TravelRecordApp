7e226c85beb51b89fcda1eacf01aabf9dd99890d
https://github.com/Browntulips <erhivwor.fortune@gmail.com>
2019-05-04 10:32:20 +0100

    Add project files.

---
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty("emailEntry");
+            bool isPasswordEmpty = string.IsNullOrEmpty("passwordEntry");
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+                Console.WriteLine("Please enter Email or Password");
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage1());
+            }
+        }
+    }
+}
